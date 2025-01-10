d376ba276e262f59b0b27578e971db98a608eea2
nojimin <sh7606555@gmail.com>
2025-01-10 11:29:56 +0900

    ✨ 출금 기록 추가
    사용자가 출금 금액을 입력하고, 잔액에서 차감되는 기능 추가, 잔액보다 큰 금액을 출력하려고 시도하면 잔액 만큼만 출금

--- atm.py
+++ atm.py
@@ -10,5 +10,11 @@ while True:
         balance = balance + deposit_amount # balance += deposit_amount
         receipts.append('입금', deposit_amount, balance)
         print(f'{deposit_amount}원 입금이 완료되었습니다. 현재 잔액은 {balance}원 입니다.')
+    if num == '2':
+        withdraw_amount = int(input("출금하실 금액을 입력해주세요 : "))
+        withdraw_amount = min(balance, withdraw_amount)
+        balance -= withdraw_amount
+        receipts.append('출금', withdraw_amount, balance)
+        print(f'{withdraw_amount}원 출금이 완료되었습니다. 현재 잔액은 {balance}원 입니다.')
 
 print(f"서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.")
