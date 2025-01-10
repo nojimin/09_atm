d00306444ef2f9f30f82a1040f95c678e03c2e5e
nojimin <sh7606555@gmail.com>
2025-01-10 11:15:05 +0900

    ✨ 입금 기능 추가
    사용자가 입금 금액을 입력하고, 잔액을 업데이트하는 기능 추가, 영수증 입금 기록을 저장합니다.

--- atm.py
+++ atm.py
@@ -1,10 +1,14 @@
-# while문을 이용해서 입금, 출금, 영수증 보기, 종료라는 기능이 종료라는 버튼을 누르기전 전까지 계속해서 노출되도록 만들어주세요
-# 종료를 누르면 서비스를 종료한다는 메시지를 출력하고 현재 잔액을 보여주세요
-
+receipts = []
 balance = 3000  #현재 잔액을 보여주세요
 
 while True:
     num = input("사용하실 기능을 선택해주세요 (1:입금, 2:출금, 3:영수증 보기, 4:종료)")
     if num == '4':
         break
+    if num == '1':
+        deposit_amount = int(input("입금하실 금액을 입력해주세요 : "))
+        balance = balance + deposit_amount # balance += deposit_amount
+        receipts.append('입금', deposit_amount, balance)
+        print(f'{deposit_amount}원 입금이 완료되었습니다. 현재 잔액은 {balance}원 입니다.')
+
 print(f"서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.")
